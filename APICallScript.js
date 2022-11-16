daa2fa218f971d9d437eed86266edab6ea313e82
Jake <jakecosio123@gmail.com>
2022-11-16 11:38:41 -0600

    initial commit

---
+++ APICallScript.js
@@ -0,0 +1,21 @@
+fetch("https://crcapi.jcosioresume.com/CRCResource",{
+    method: "POST",
+    body: JSON.stringify({})
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        displayCount(data)
+    })
+    .catch(err => {
+        console.log('error ${err}')
+    })
+
+    function displayCount(data) {
+        const count = data.ddbResponse.Attributes;
+        const countDiv = document.getElementById("VisitorCount");
+        const countName = count.VisitorCount;
+        const heading = document.createElement("h1");
+        heading.innerHTML = countName;
+        countDiv.appendChild(heading);
+    }
