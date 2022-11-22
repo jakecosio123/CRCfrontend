cfca0b8df44f402be10dbc87ddc2ed1bd36f7af6
Jake <jakecosio123@gmail.com>
2022-11-21 21:54:14 -0600

    fix cypress test

--- cypress/e2e/ResumeCountTest.js
+++ cypress/e2e/ResumeCountTest.js
@@ -13,3 +13,20 @@
 //     })
 //   })
 // })
+
+describe('API Test', () => {
+    it('Tests the API Response', () => {
+        cy.request('POST', 'https://crcapi.jcosioresume.com/CRCResource', {})
+       .should((response1) => {
+            cy.log(JSON.stringify(response1.body)).then(response1.body) => {
+                const count1 = parseFloat(response1.body.ddbResponse.Attributes.VisitorCount)
+            }})
+            cy.wait(2000)
+            cy.request('POST', 'https://crcapi.jcosioresume.com/CRCResource', {})
+            .should((response2) => {
+                 cy.log(JSON.stringify(response2.body)).then(response2.body) => {
+                     const count2 = parseFloat(response2.body.ddbResponse.Attributes.VisitorCount)
+                 }})
+            expect(count2).to.be.greaterThan(count1)
+        })
+    })    
